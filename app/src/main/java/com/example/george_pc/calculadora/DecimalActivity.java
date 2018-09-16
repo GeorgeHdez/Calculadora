9ba78b4f2775420fdd232e8a217f0b60f237ae3f
GeorgeHdez <jgarcia148@alumnos.uaq.mx>
2018-09-16 17:44:55 -0500

    Commit

--- app/src/main/java/com/example/george_pc/calculadora/DecimalActivity.java
+++ app/src/main/java/com/example/george_pc/calculadora/DecimalActivity.java
@@ -3,6 +3,8 @@ package com.example.george_pc.calculadora;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+//Aplicacion de una calculadora con multiples converciones de base.
+
 public class DecimalActivity extends AppCompatActivity {
 
     @Override
