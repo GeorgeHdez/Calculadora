b4b58b72a7db855762dd452076cad95888b12266
GeorgeHdez <jgarcia148@alumnos.uaq.mx>
2018-09-16 18:36:56 -0500

    Plantilla decimal con switches de cambio de base.

--- app/src/main/java/com/example/george_pc/calculadora/DecimalActivity.java
+++ app/src/main/java/com/example/george_pc/calculadora/DecimalActivity.java
@@ -2,14 +2,12 @@ package com.example.george_pc.calculadora;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-//Aplicacion de una calculadora con multiples converciones de base.
-
 public class DecimalActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_decimal);
+
     }
 }
